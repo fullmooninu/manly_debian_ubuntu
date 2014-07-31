29373781b446675eb1cfed6ac5145d6bff467dd1
Miguel <fullmooninu@users.noreply.github.com>
2014-07-31 05:53:15 +0100

    Create shutdown.bash
    The manly shutdown file. Just run. Description of what it does in comments.

---
+++ shutdown.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -v
+
+  #Requires user with sudo password
+
+  #Distribution upgrade
+sudo apt-get update;
+sudo apt-get dist-upgrade -y --fix-missing --fix-broken;
+
+  #Erase old downloaded archive files
+sudo apt-get autoclean -y;
+
+  #Remove automatically all unused packages
+sudo apt-get autoremove -y;
+
+  #Shutdown
+sudo poweroff
+#for restart use instead:
+#sudo reboot
