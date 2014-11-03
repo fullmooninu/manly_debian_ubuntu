9985b292d28a1d66afa438ec28eb26c8b8cbc19f
Miguel <fullmooninu@users.noreply.github.com>
2014-11-03 17:49:32 +0000

    Update postInstall.bash

--- postInstall.bash
+++ postInstall.bash
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
 set -v
 
-now, where i put that file...?
+now, where i put that file with the beta version...?
+
+
+# activate all repositories
+
+# add repositories if needed
+
+# dist-update
+
+# install recommended drivers automatically
+
+# dist-upgrade with suggests (test if --install-suggests has the same effect as that @install-suggests option for apt-get)
+
+# install fonts and other media ubuntu shtuff
+
+# install shtuff (skype etc, make a list) (all with suggests)
+
+# you probably dont want to use ubuntu ppa, deb stuff seems more reliable.
