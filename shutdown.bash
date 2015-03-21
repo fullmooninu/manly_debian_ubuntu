6e196b2d1de47a89aade472ecc4d949e942a058f
Miguel <fullmooninu@users.noreply.github.com>
2015-03-21 13:30:27 +0000

    Update shutdown.bash

--- shutdown.bash
+++ shutdown.bash
@@ -8,7 +8,7 @@ sudo apt-get update;
 sudo apt-get dist-upgrade -y --fix-missing --fix-broken --purge;
 
   #Erase old downloaded archive files
-sudo apt-get autoclean -y --purge;
+sudo apt-get autoclean -y;
 
   #Remove automatically all unused packages
 sudo apt-get autoremove -y --purge;
