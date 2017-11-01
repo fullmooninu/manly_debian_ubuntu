d1b182d0165f4b09ef083a4663e499913414cf72
Miguel <fullmooninu@users.noreply.github.com>
2017-11-01 23:47:56 +0000

    Update postInstall.bash

--- postInstall.bash
+++ postInstall.bash
@@ -24,8 +24,7 @@ sudo apt-get install -y steam chromium-browser hexchat pidgin ntp workrave
 sudo apt-get install -y idle3 haskell-platform
 
 ##Sublime3 : https://www.sublimetext.com/docs/3/linux_repositories.html
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg
-sudo apt-key add sublimehq-pub.gpg
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install -y sublime-text
