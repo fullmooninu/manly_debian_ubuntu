aa94f2b6d17a3af0b6e044c1faf18d57357b075b
Miguel <fullmooninu@users.noreply.github.com>
2017-11-01 23:15:25 +0000

    Update postInstall.bash

--- postInstall.bash
+++ postInstall.bash
@@ -9,6 +9,8 @@ set -v
 sudo apt-get update
 sudo apt install -y ntp
 sudo apt-get dist-upgrade -y --fix-missing --fix-broken --purge
+##Languages https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+sudo apt-get install language-pack-pt language-pack-fr language-pack-zh-hans language-pack-zh-hant language-pack-ja language-pack-en
 sudo apt install $(check-language-support)
 ##Standard stuff everyone should have
 sudo apt install -y wget curl apt-transport-https git
@@ -19,13 +21,13 @@ sudo apt install -y steam chromium-browser hexchat pidgin ntp workrave
 #sudo apt install -y intel-microcode # if not intel disable this!!
 
 ##Workstuff
-sudo apt install -y idle3
+sudo apt install -y idle3 haskell-platform
 
 ##Sublime3 : https://www.sublimetext.com/docs/3/linux_repositories.html
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -y -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
-sudo apt-get install sublime-text
+sudo apt install sublime-text
 ##Keybase
 curl -O https://prerelease.keybase.io/keybase_amd64.deb
 # if you see an error about missing `libappindicator1`
