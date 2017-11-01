852a23097386d5af217529f3a65fe5b7431bd98a
Miguel <fullmooninu@users.noreply.github.com>
2017-11-01 22:26:38 +0000

    Update shutdown.bash

--- shutdown.bash
+++ shutdown.bash
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 set -v
 
-  #Requires user with sudo password
+##Requires user with sudo password
 
-  #Distribution upgrade
+##Distribution upgrade
 sudo apt-get update
 sudo apt-get dist-upgrade -y --fix-missing --fix-broken --purge
 
-  #Erase old downloaded archive files
+##Erase old downloaded archive files
 sudo apt-get autoclean -y
 
-  #Remove automatically all unused packages
+##Remove automatically all unused packages
 sudo apt-get autoremove -y --purge
 
-  #Shutdown
+##Shutdown
 sudo poweroff
 #for restart use instead:
 #sudo reboot
