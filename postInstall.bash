5716141949c6917f874d9a0e1f828e40561f06a9
Miguel <fullmooninu@users.noreply.github.com>
2018-05-15 15:43:57 +0100

    Update postInstall.bash

--- postInstall.bash
+++ postInstall.bash
@@ -21,8 +21,8 @@ sudo apt-get install -y ssh nano lynis glances ncdu synaptic
 sudo apt-get install -y steam chromium-browser hexchat pidgin workrave
 #sudo apt-get install -y intel-microcode # if not intel disable this!!
 
-##Workstuff
-sudo apt-get install -y idle3 haskell-platform default-jdk-headless default-jre-headless
+##Python and java
+sudo apt-get install -y python3 idle3 default-jdk default-jre
 
 ##Sublime3 : https://www.sublimetext.com/docs/3/linux_repositories.html
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
