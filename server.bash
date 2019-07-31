2f499dcf4563104755b2f5dcd45ea5b26f49ca8c
Miguel <fullmooninu@users.noreply.github.com>
2019-07-31 16:14:01 +0100

    Update server.bash

--- server.bash
+++ server.bash
@@ -2,5 +2,24 @@ apt install ntp -y
 apt install build-essentials -y 
 apt install curl wget apt-transport-https dirmngr -y
 apt install fail2ban -y
+apt install sudo -y
+apt install  ssh -y
 
-apt install  ssh
+
+useradd -m -d /home/ssarah ssarah
+addgroup ssarah sudo
+
+#create user
+#change password access
+#change root password
+
+
+#ssh config
+#remove root login
+#remove ssh access through password
+
+
+
+
+#apt install ufw
+#allow http https ssh different port
